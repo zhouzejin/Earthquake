5c880ae6d722cf8e5435eea94ad99fa6e5e943a9
Zhou Zejin <zjzhou1@chinaums.com>
2015-10-30 18:00:50 +0800

    开始9.3.3 使用重复Alarm调度网络刷新

---
+++ src/com/sunny/earthquake/EarthquakeAlarmReceiver.java
@@ -0,0 +1,18 @@
+package com.sunny.earthquake;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class EarthquakeAlarmReceiver extends BroadcastReceiver {
+
+	public static final String ACTION_REFRESH_EARTHQUAKE_ALARM = 
+			"com.sunny.earthquake.ACTION_REFRESH_EARTHQUAKE_ALARM";
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		Intent startIntent = new Intent(context, EarthquakeUpdateService.class);
+		context.startService(startIntent);
+	}
+
+}
